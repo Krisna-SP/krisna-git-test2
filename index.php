82ea4e9b8f4e04b04333c8089c6ad45b8e241596
Krisna-SP <krisnasuryap62@gmail.com>
2021-08-01 11:53:44 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda optio ducimus sint ut at distinctio ipsa maxime repellendus quis nobis! Sit dicta quas hic eligendi in possimus vel debitis. Exercitationem.</p>
 </body>
 </html>
